ae0d13d74c5dadf422a9f2407d0588e740b9b416
Ricco <103573129+riccovi@users.noreply.github.com>
2024-08-14 09:52:14 +0200

    Added R to Restart

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -31,11 +32,16 @@ public class GameManager : MonoBehaviour
     }
 
     // Update is called once per frame
+
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
     }
 
+    
     public void UpgradeDoubleJump()
     {
         PlayerMove.ExtraJumps=1;
