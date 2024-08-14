dc720dbe4cdb3a5c829afae9624cfefa79058700
Ricco <103573129+riccovi@users.noreply.github.com>
2024-08-14 11:15:52 +0200

    PlayerCollision Script

---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerCollision : MonoBehaviour
+{
+    private SpriteRenderer playerRenderer;
+
+    private void Start()
+    {
+        Transform spriteTransform = transform.Find("PlayerGraphics/Sprite");
+        playerRenderer = spriteTransform.GetComponent<SpriteRenderer>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Nails"){
+            playerRenderer.color = Color.red;
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
