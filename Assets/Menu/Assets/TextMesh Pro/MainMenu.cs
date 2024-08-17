de07bdbea740127afb733c56595890dc3686daa9
Maxime <mmlecocq@gmail.com>
2024-08-17 18:25:15 +0200

    Added menu functionality
    Added menu functionality

---
+++ Assets/Menu/Assets/TextMesh Pro/MainMenu.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public GameObject mainMenu;
+    public GameObject settingsMenu;
+    public GameObject creditsMenu;
+    public GameObject backButton;
+
+    void Start()
+    {
+        mainMenu.SetActive(true);
+        settingsMenu.SetActive(false);
+        creditsMenu.SetActive(false);
+        backButton.SetActive(false);
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void GoToSettingsMenu()
+    {
+        mainMenu.SetActive(false);
+        settingsMenu.SetActive(true);
+        backButton.SetActive(true);    
+    }
+
+    public void GoToCreditsMenu()
+    {
+        mainMenu.SetActive(false);
+        creditsMenu.SetActive(true);
+        backButton.SetActive(true);
+    }
+
+    public void GoToMainMenu()
+    {
+        mainMenu.SetActive(true);
+        settingsMenu.SetActive(false);
+        creditsMenu.SetActive(false);
+        backButton.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit Game");
+        Application.Quit();
+    }
+}
---
+++ Assets/Menu/Assets/TextMesh Pro/SettingsMenu.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public Slider volumeSlider;
+    public TextMeshProUGUI volumeLabel;
+
+    void Start()
+    {
+        volumeSlider.value = AudioListener.volume;
+        volumeLabel.text = "Volume: " + (volumeSlider.value * 100).ToString("0");
+
+        volumeSlider.onValueChanged.AddListener(delegate { OnVolumeChange(); });
+    }
+
+    public void OnVolumeChange()
+    {
+        AudioListener.volume = volumeSlider.value;
+        volumeLabel.text = "Volume: " + (volumeSlider.value * 100).ToString("0");
+    }
+}
