6ca7199bfc4486432498b1a1dc1c8c7dc8523e9e
GO <golee.dev@gmail.com>
2024-09-03 11:08:59 +0900

    reset hard

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -66,16 +66,11 @@ public class GameManager : MonoBehaviour
     }
 
     // Update is called once per frame
-
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.R))
-        {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-        }
+        
     }
 
-    
     public void UpgradeDoubleJump()
     {
         PlayerMove.ExtraJumps=1;
